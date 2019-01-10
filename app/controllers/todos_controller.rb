1a41a7f91856a318528a4787fe3f4db43e7d53c1
suman saurabh <suman.saurabh@grabonrent.com>
2019-01-10 15:50:57 +0530

    added mock user class for code refactoring

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,15 +2,15 @@ class TodosController < ApplicationController
   before_action :authenticate
 
   def index
-    @todos = Todo.where(email: current_email)
-	end
+    @todos = current_user.todos
+  end
 
   def new
     @todo = Todo.new
   end
 
   def create
-    Todo.create(todo_params.merge(email: current_email))
+    current_user.todos.create(todo_params)
     redirect_to todos_path
   end
 
